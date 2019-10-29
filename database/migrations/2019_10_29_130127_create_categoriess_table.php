31948e669e64813a6f6a08b0ced46d1e32c8db43
andres <andres_constantino_pinon_apps1ma1819@cev.com>
2019-10-29 14:06:38 +0100

    // hecha la segunda migración, corregidos los tipos de variable de la primera migración

--- database/migrations/2019_10_29_130127_create_categoriess_table.php
+++ database/migrations/2019_10_29_130127_create_categoriess_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCategoriessTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categoriess', function (Blueprint $table) {
             $table->increments('id');
             $table->String('name');
-            $table->String('email');
-            $table->String('password');           
+            $table->integer('id_user')->unsigned();
+            $table->foreign('id_user')->references('id')->on('users');
             $table->timestamps();
+            
         });
     }
 
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categoriess');
     }
 }
